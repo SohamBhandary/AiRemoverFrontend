72c7c9d77c08bcdf22088e6a7ea4456c6a845ab8
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-09 15:47:10 +0530

    Ui for BgRemovalSteps Done

--- src/Components/BgRemovalSteps.jsx
+++ src/Components/BgRemovalSteps.jsx
@@ -9,9 +9,21 @@ const BgRemovalSteps = () => {
 
 
         </h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
-            {steps.map((step,index)=>(
-                <div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8  cursor-pointer'>
+            {steps.map((item,index)=>(
+                <div key={index} className='bg-gray50 p-8 rounded-2xl shadow-sm' >
+                  <span className='inline-block bg-gray-200 text-indigo-800 text-sm font-semibold  px-3 py-1 rounded-full mb-4' >
+{item.step}
+
+
+                  </span>
+                  <h3 className='text-xl font-bold text-gray-900 mb-4'>
+{item.title}
+                  </h3>
+                  <p className='text-gray-600 text-base leading-relaxed'>
+{item.description}
+
+                  </p>
 
                     
                 </div>
